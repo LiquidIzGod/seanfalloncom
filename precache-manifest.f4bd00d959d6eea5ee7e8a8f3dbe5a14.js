e391f85c627177956170b6f7975dfca5a1464cb1
James Tupper <tupper.james@gmail.com>
2024-07-15 17:00:22 -0700

    Updates

---
+++ precache-manifest.f4bd00d959d6eea5ee7e8a8f3dbe5a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9d84c01b9821504e36353e34530816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04cab2ae698928d41130",
+    "url": "/static/css/main.e2360cad.chunk.css"
+  },
+  {
+    "revision": "a531dea4f05055076241",
+    "url": "/static/js/2.a7dbd842.chunk.js"
+  },
+  {
+    "revision": "04cab2ae698928d41130",
+    "url": "/static/js/main.c22c1234.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
